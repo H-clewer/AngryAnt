361bd2859ea7c14a09a3f8df837ce4bd8b178b0c
H-clewer <67416910+H-clewer@users.noreply.github.com>
2021-08-11 01:24:54 +0100

    Mobile Updates
    added sidepanel nav for mobile

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
+            console.log('tetso');
         }
         else
         {
             $('.navbar').removeClass("sticky");
         }
     })
+
+    //toggle navbar on small screens
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        console.log('harley');
+    });
 });
